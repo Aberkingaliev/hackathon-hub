d0dfc1ba850d53464670a19456b559181bfef89f
Aberkingaliev <erkingaliev21@gmail.com>
2023-09-04 01:52:05 +0600

    Added untraceable files

---
+++ service-team/src/main/resources/db/migration/V1__Create_team_table.sql
@@ -0,0 +1,9 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE teams (
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+    name VARCHAR(255) NOT NULL UNIQUE,
+    description VARCHAR(255) NOT NULL,
+    founder_id UUID NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+)
