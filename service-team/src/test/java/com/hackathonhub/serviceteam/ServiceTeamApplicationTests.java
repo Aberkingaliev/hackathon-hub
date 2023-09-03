f803d49ae4c0e6e7297f906a9d30d867cfb69b2c
Aberkingaliev <erkingaliev21@gmail.com>
2023-09-04 01:15:37 +0600

    Fix

---
+++ service-team/src/test/java/com/hackathonhub/serviceteam/ServiceTeamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathonhub.serviceteam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceTeamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
