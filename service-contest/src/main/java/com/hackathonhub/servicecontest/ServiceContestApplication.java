bc3a8513690a6cb81eadff295861074b148dc987
Aberkingaliev <erkingaliev21@gmail.com>
2023-09-15 00:49:34 +0600

    Initialize service-contest

---
+++ service-contest/src/main/java/com/hackathonhub/servicecontest/ServiceContestApplication.java
@@ -0,0 +1,13 @@
+package com.hackathonhub.servicecontest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceContestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceContestApplication.class, args);
+	}
+
+}
---
+++ service-contest/src/test/java/com/hackathonhub/servicecontest/ServiceContestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathonhub.servicecontest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceContestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
